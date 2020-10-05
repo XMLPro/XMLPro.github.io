76d0587287c11852ae3a95a69293db2cb2641c3a
hakomori64 <hakomori64@gmail.com>
2020-10-05 22:36:54 +0900

    add script

---
+++ README.md
@@ -0,0 +1,26 @@
+# hugoのインストール
+
+公式のドキュメントを参考にローカルにhugoをインストールしてください
+(https://gohugo.io/getting-started/installing/)[https://gohugo.io/getting-started/installing/]
+
+# 記事の書き方
+
+ブログ記事、XMLProに関するニュース、その他投稿の3種類の記事をマークダウンで書くことが出来ます。
+
+### ブログ
+
+```bash
+./new.sh blogs  
+```
+
+### XMLProに関するニュース
+
+```bash
+./new.sh news
+```
+
+### その他の投稿
+
+```bash
+./new.sh posts
+```
---
+++ new.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Few Argument"
+  echo "USAGE: $0 blogs|news|posts"
+  exit 1
+fi
+if [ $1 != "blogs" ] && [ $1 != "posts" ] && [ $1 != "news" ]; then
+  echo "Invalid Argument"
+  echo "USAGE: $0 blogs|news|posts"
+  exit 1
+fi
+
+filename=$(date "+%Y-%m-%d-%H%M").md
+hugo new $1/$filename
+echo "Congratulations!!"
+echo "content/$1/$filename Created!"
+echo "Edit and Deploy!"
